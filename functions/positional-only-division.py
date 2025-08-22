d17fccb2cf698d01f54e825dea7aed3455f3956d
KhanMarshaI <iamtaha05@gmail.com>
2025-08-22 14:55:28 +0500

    Create positional_only_division

--- functions/positional-only-division.py
+++ functions/positional-only-division.py
@@ -1,3 +1,13 @@
 # Positional-only Division
 # Define def div(a, b, /, precision=2) that forbids keyword use of a and b but allows precision=4.
 
+from decimal import Decimal, localcontext
+
+def div(a,b, /, precision=2):
+    with localcontext() as ctx:
+        ctx.prec = precision
+        result = Decimal(a) / Decimal(b)
+        return result
+
+
+print(div(22,7, precision=15))
